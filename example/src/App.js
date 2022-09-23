33ed84443038f92751c1d2dda3f60f8b65eafabd
Kcalita <keyur.alitainfotech@gmail.com>
2022-09-23 11:56:27 +0530

    updated examplecomponent

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-sociallogin-kc'
+import ExampleComponent from '../../src'
 import 'react-sociallogin-kc/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library Example 😄' />
 }
 
 export default App
