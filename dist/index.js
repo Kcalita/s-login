e11a61bd1f0a3940ad686dc3e777d1e6fb3c2920
Kcalita <keyur.alitainfotech@gmail.com>
2022-09-27 16:44:11 +0530

    dist file have been saved

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,20 @@
-function _interopDefault(ex) {
-  return ex && typeof ex === 'object' && 'default' in ex ? ex['default'] : ex
-}
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var google = require('@react-oauth/google')
-var React = _interopDefault(require('react'))
+var google = require('@react-oauth/google');
+var React = _interopDefault(require('react'));
 
 function Glogin() {
-  return /*#__PURE__*/ React.createElement(
-    google.GoogleOAuthProvider,
-    {
-      clientId:
-        '562677907760-7m99omjeao1ebmgn1ms024mp3pufpcmj.apps.googleusercontent.com'
+  return /*#__PURE__*/React.createElement(google.GoogleOAuthProvider, {
+    clientId: "562677907760-7m99omjeao1ebmgn1ms024mp3pufpcmj.apps.googleusercontent.com"
+  }, /*#__PURE__*/React.createElement(google.GoogleLogin, {
+    onSuccess: function onSuccess(credentialResponse) {
+      console.log('login successfull---------->', credentialResponse);
     },
-    /*#__PURE__*/ React.createElement(google.GoogleLogin, {
-      onSuccess: function onSuccess(credentialResponse) {
-        console.log('login successfull---------->', credentialResponse)
-      },
-      onError: function onError() {
-        console.log('login failed')
-      }
-    })
-  )
+    onError: function onError() {
+      console.log('login failed');
+    }
+  }));
 }
 
-module.exports = Glogin
+module.exports = Glogin;
 //# sourceMappingURL=index.js.map
--- dist/index.modern.js
+++ dist/index.modern.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Glogin() {
   return /*#__PURE__*/React.createElement(GoogleOAuthProvider, {
-    clientId: "YOUR_CLIENT_ID"
+    clientId: "562677907760-7m99omjeao1ebmgn1ms024mp3pufpcmj.apps.googleusercontent.com"
   }, /*#__PURE__*/React.createElement(GoogleLogin, {
     onSuccess: function onSuccess(credentialResponse) {
       console.log('login successfull---------->', credentialResponse);
